cb3fb9eb55215beff0fd09ccc2249bf53f7fe471
Dustin <xatrekak@gmail.com>
2024-01-05 21:14:48 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/bash
 
-# sudo yum -y install https://extras.getpagespeed.com/release-latest.rpm
-# sudo yum -y install lastversion
+array=( https://extensions.gnome.org/extension/307/dash-to-dock/ )
 
-# lastversion download gorhill/uBlock  --format assets --filter firefox.xpi -o uBlock.xpi
+for i in "${array[@]}"
+do
+    EXTENSION_ID=$(curl -s $i | grep -oP 'data-uuid="\K[^"]+')
+    VERSION_TAG=$(curl -Lfs "https://extensions.gnome.org/extension-query/?search=$EXTENSION_ID" | jq '.extensions[0] | .shell_version_map | map(.pk) | max')
+    wget -O ${EXTENSION_ID}.zip "https://extensions.gnome.org/download-extension/${EXTENSION_ID}.shell-extension.zip?version_tag=$VERSION_TAG"
+    gnome-extensions install --force ${EXTENSION_ID}.zip
+    if ! gnome-extensions list | grep --quiet ${EXTENSION_ID}; then
+        busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${EXTENSION_ID}
+    fi
+    gnome-extensions enable ${EXTENSION_ID}
+    rm ${EXTENSION_ID}.zip
+done
+
+gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
+gsettings set org.gnome.shell favorite-apps ['org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'firefox.desktop', 'com.visualstudio.code.desktop', 'com.discordapp.Discord.desktop', 'org.gnome.Software.desktop']
 
 wget https://addons.mozilla.org/firefox/downloads/file/4198829/ublock_origin-latest.xpi
 wget https://addons.mozilla.org/firefox/downloads/file/4208799/lastpass_password_manager-latest.xpi
