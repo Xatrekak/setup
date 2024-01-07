de45612245a66ae3f5ebdd47c23467818153517c
Dustin <xatrekak@gmail.com>
2024-01-07 01:50:17 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 
 #install missing user gnome extensions
 array=( https://extensions.gnome.org/extension/307/dash-to-dock/
-https://extensions.gnome.org/extension/517/caffeine/ )
+https://extensions.gnome.org/extension/517/caffeine/
+https://extensions.gnome.org/extension/4362/fullscreen-avoider/ )
 
 for i in "${array[@]}"
 do
@@ -37,23 +38,41 @@ sudo bash -c "echo '192.168.1.81:/mnt/tank/dustin    /mnt/nas    nfs    defaults
 sudo systemctl daemon-reload
 sudo mount /mnt/nas
 
-#Add NAS to gnome file browser
-echo 'file:///mnt/nas' >> /home/dustin/.config/gtk-3.0/bookmarks
+#Setup gnome file browser
+echo 'file:///mnt/nas' >> ~/.config/gtk-3.0/bookmarks
+touch ~/Templates/script.sh
+echo -e '#!/bin/bash\n' > ~/Templates/script.sh
+mkdir ~/Projects
+echo file://$HOME/Projects >> ~/.config/gtk-3.0/bookmarks
 
 #Add bookmarks to Firefox
 cp /mnt/nas/firefox/places.sqlite ~/.mozilla/firefox/bysvtelr.default-release/places.sqlite
 
 #install and setup flatpaks
 flatpak install flathub com.visualstudio.code
-touch /home/dustin/.var/app/com.visualstudio.code/config/electron-flags.conf
-echo "--enable-features=UseOzonePlatform,WaylandWindowDecorations" >> /home/dustin/.var/app/com.visualstudio.code/config/electron-flags.conf
-echo "--ozone-platform=wayland" >> /home/dustin/.var/app/com.visualstudio.code/config/electron-flags.conf
+touch ~/.var/app/com.visualstudio.code/config/electron-flags.conf
+echo "--enable-features=UseOzonePlatform,WaylandWindowDecorations" >> ~/.var/app/com.visualstudio.code/config/electron-flags.conf
+echo "--ozone-platform=wayland" >> ~/.var/app/com.visualstudio.code/config/electron-flags.conf
 
-#Enable and setup gnome extensions 
+#Enable and setup system gnome extensions 
 gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
 gsettings set org.gnome.shell favorite-apps ['org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'firefox.desktop', 'com.visualstudio.code.desktop', 'com.discordapp.Discord.desktop', 'org.gnome.Software.desktop']
 gsettings set org.gnome.nautilus.preferences always-use-location-entry true
 
-#Create symlink to t-rex miner can access NVML to monitor GPU
+#setup gnome user extensions
+#dash-to-doc
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock multi-monitor true
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock show-mounts false
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock show-mounts-network false
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock show-show-apps-button false
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock show-trash false
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock disable-overview-on-startup true
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock apply-custom-theme true
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
+#caffeine
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/caffeine\@patapon.info/schemas/ set org.gnome.shell.extensions.caffeine screen-blank 'always'
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/caffeine\@patapon.info/schemas/ set org.gnome.shell.extensions.caffeine show-indicator 'always'
+
+#Create symlink to libnvidia-ml.so.1 so t-rex miner can access NVML to monitor GPU
 sudo ln -s /usr/lib/libnvidia-ml.so.1 /usr/lib/libnvidia-ml.so
 sudo ln -s /usr/lib64/libnvidia-ml.so.1 /usr/lib64/libnvidia-ml.so
